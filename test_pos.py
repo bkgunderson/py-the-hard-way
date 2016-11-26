0611bc44c35e9706693a212812181cc9565b244d
bkgunderson <brian@bkgunderson.com>
2016-11-26 10:07:05 -0600

    few more small additions to PoS file

--- test_pos.py
+++ test_pos.py
@@ -1,4 +1,5 @@
 import food_items
+MO_FOODTAX = 0.05257
 
 
 def receipt(food, nonfood):
@@ -6,8 +7,8 @@ def receipt(food, nonfood):
     nonfood_sub = float(nonfood * 3.99)
     sub_total = nonfood_sub + food_sub_total
     nonfood_tax = float(nonfood_sub * 0.1278)
-    food_tax = float(food_sub_total * 0.05257)
-    total = nonfood_sub + nonfood_tax + food_sub_total + food_tax
+    food_tax = float(food_sub_total * MO_FOODTAX)
+    total = sub_total + nonfood_tax + food_tax
     print "   Sub Total     $%s" % sub_total
     print "Non Food Tax     $%s" % nonfood_tax
     print "    Food Tax     $%s" % food_tax
@@ -16,7 +17,16 @@ def receipt(food, nonfood):
 print "How many 4-packs of Boulevard Breakfast Stout are you buying?"
 stout = int(raw_input("> "))
 
+print "How many bottles of Chilean Cabernet Sauvignon are you buying?"
+cab_sauv = int(raw_input("> "))
+
+print "How many 34 oz bottles of Olive Oil are you buying?"
+evoo = int(raw_input("> "))
+
+print "How many pounds of dry green lentils are you buying?"
+grn_lent = float(raw_input("> "))
+
 print "How many non-food items are you buying?"
 non_food_qty = int(raw_input("> "))
 
-receipt((stout, 1, 1, 1), non_food_qty)
+receipt((stout, cab_sauv, evoo, grn_lent), non_food_qty)
