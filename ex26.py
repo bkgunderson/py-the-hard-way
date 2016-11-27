3d97ac2d7bcdcea2203cf06a1a5776fb853dcb70
bkgunderson <brian@bkgunderson.com>
2016-11-27 16:42:29 -0600

    some cleanup to ex26.py

--- ex26.py
+++ ex26.py
@@ -1,34 +1,45 @@
 import ex25
 
+
 def break_words(stuff):
-    """This function will break up words for us."""
+    """
+    This function will break up words for us.
+    :param: stuff
+    :return: words
+    """
     words = stuff.split(' ')
     return words
+
 
 def sort_words(words):
     """Sorts the words."""
     return sorted(words)
+
 
 def print_first_word(words):
     """Prints the first word after popping it off."""
     word = words.pop(0)
     print word
 
+
 def print_last_word(words):
     """Prints the last word after popping it off."""
     word = words.pop(-1)
     print word
+
 
 def sort_sentence(sentence):
     """Takes in a full sentence and returns the sorted words."""
     words = break_words(sentence)
     return sort_words(words)
 
+
 def print_first_and_last(sentence):
     """Prints the first and last words of the sentence."""
     words = break_words(sentence)
     print_first_word(words)
     print_last_word(words)
+
 
 def print_first_and_last_sorted(sentence):
     """Sorts the words then prints the first and last one."""
@@ -57,11 +68,12 @@ print "--------------"
 five = 10 - 3 + 3 - 5
 print "This should be five: %s" % five
 
+
 def secret_formula(started):
     jelly_beans = started * 5000
-    jars = jelly_beans / 100
-    crates = jars / 10
-    return jelly_beans, jars, crates
+    jelly_jars = jelly_beans / 100
+    jelly_crates = jelly_jars / 10
+    return jelly_beans, jelly_jars, jelly_crates
 
 
 start_point = 10000
@@ -70,7 +82,7 @@ beans, jars, crates = secret_formula(start_point)
 print "With a starting point of: %d" % start_point
 print "We'd have %d beans, %d jars, and %d crates." % (beans, jars, crates)
 
-start_point = start_point / 10
+start_point /= 10
 
 print "We can also do that this way:"
 print "We'd have %d beans, %d jars, and %d crates." % secret_formula(start_point * 10)
@@ -79,8 +91,6 @@ print "We'd have %d beans, %d jars, and %d crates." % secret_formula(start_point
 sentence = "All good things come to those who wait."
 
 words = ex25.break_words(sentence)
-sorted_words = ex25.sort_words(words)
-
 print_first_word(words)
 print_last_word(words)
 sorted_words = ex25.sort_sentence(sentence)
